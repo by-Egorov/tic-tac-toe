d34c46ab3b5bc25a0494d330d88e1c35e45f8cb3
Alexander <egorov.dev@gmail.com>
2022-10-18 02:12:22 +0300

    fun

--- app.js
+++ app.js
@@ -1,24 +1,158 @@
-const board = document.querySelector('.board')
-const newGame = document.querySelector('.new-game')
-let counter = 0
+let ceil = document.getElementsByClassName('game-item')
+let reset = document.getElementById('reset-game')
+let message = document.getElementById('message')
+let messages = document.getElementById('messages')
+let player = 'X'
+let stepCount = 0
+let winCombinations = [ [1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9] ]
+let dataX = []
+let dataO = []
 
-board.addEventListener('click', function (event) {
-	let target = event.target
-	start(target)
-})
-
-function start(item) {
-	counter++
-	if (counter === 1) {
-		item.textContent = 'X'
-		item.classList.add('bcg-active-cross')
-		return
-	} else {
-		item.textContent = 'O'
-		counter = 0
-		item.classList.add('zero', 'bcg-active-zero')
-	}
+// Обработчик на каждый элемент(цикл for)
+for (let i = 0; i < ceil.length; i++) {
+  ceil[i].addEventListener('click', currentStep)
 }
-newGame.addEventListener('click', () => {
-	location.reload()
+
+// +this(преобразовывает в число) значение атрибута куда кликнули
+function currentStep() {
+  let num = +this.getAttribute("data-ceil")
+  if (!this.textContent) {
+    this.innerText = player
+    player === "X" ? dataX.push(num) && this.classList.add("x") : dataO.push(num) && this.classList.add("o")
+    if (
+      (dataO.length > 2 || dataX.length > 2) &&
+      (checkWin(dataO, num) || checkWin(dataX, num))
+    ) {
+      for (let i = 0; i < ceil.length; i++) {
+        ceil[i].removeEventListener("click", currentStep)
+      }
+      return (message.innerText = "Победил игрок " + player)
+    }
+    changePlayer()var ceil = document.getElementsByClassName("game-item"),
+      reset = document.getElementById("reset-game"),
+      message = document.getElementById("message"),
+      player = "X",
+      stepCount = 0,
+      winCombinations = [
+        [1, 2, 3],
+        [1, 4, 7],
+        [1, 5, 9],
+        [2, 5, 8],
+        [3, 6, 9],
+        [3, 5, 7],
+        [4, 5, 6],
+        [7, 8, 9]
+      ],
+      dataX = [],
+      dataO = [];
+    
+    for (var i = 0; i < ceil.length; i++) {
+      ceil[i].addEventListener("click", currentStep);
+    }
+    
+    function currentStep() {
+      var num = +this.getAttribute("data-ceil");
+      if (!this.textContent) {
+        this.innerText = player;
+        player === "X"
+          ? dataX.push(num) && this.classList.add("x")
+          : dataO.push(num) && this.classList.add("o");
+        if (
+          (dataO.length > 2 || dataX.length > 2) &&
+          (checkWin(dataO, num) || checkWin(dataX, num))
+        ) {
+          for (var i = 0; i < ceil.length; i++) {
+            ceil[i].removeEventListener("click", currentStep);
+          }
+          return (message.innerText = "Победил игрок " + player);
+        }
+        changePlayer();
+        stepCount++;
+        stepCount === 9
+          ? (message.innerText = "Ничья")
+          : (message.innerText = "Ходит игрок " + player);
+      }
+    }
+    
+    function changePlayer() {
+      player === "X" ? (player = "O") : (player = "X");
+    }
+    
+    reset.addEventListener("click", function() {
+      for (var i = 0; i < ceil.length; i++) {
+        ceil[i].innerText = "";
+      }
+      dataO = [];
+      dataX = [];
+      player = "O";
+      stepCount = 0;
+      message.innerText = "Ходит игрок " + player;
+      for (var i = 0; i < ceil.length; i++) {
+        ceil[i].addEventListener("click", currentStep);
+        ceil[i].classList.remove("x", "o");
+      }
+    });
+    
+    function checkWin(arr, number) {
+      for (var w = 0, wLen = winCombinations.length; w < wLen; w++) {
+        var someWinArr = winCombinations[w],
+          count = 0;
+        if (someWinArr.indexOf(number) !== -1) {
+          for (var k = 0, kLen = someWinArr.length; k < kLen; k++) {
+            if (arr.indexOf(someWinArr[k]) !== -1) {
+              count++;
+              if (count === 3) {
+                return true;
+              }
+            }
+          }
+          count = 0;
+        }
+      }
+    }
+    stepCount++
+    stepCount === 9
+      ? (messages.innerText = "Ничья")
+      : (message.innerText = "Ходит игрок " + player)
+  }
+}
+
+function changePlayer () {
+  player === 'X' ? player = 'O' : player = 'X'
+  // Проверяет, если первый игрок 'X', тогда ходит игрок 'О' иначе ходит игрок Х
+}
+
+reset.addEventListener('click', () => {
+  for (let i = 0; i < ceil.length; i++) {
+    ceil[i].addEventListener('click', currentStep)
+    ceil[i].innerText = ''
+  }
+  dataX = []
+  dataO = []
+  player = 'X'
+  stepCount = 0
+  messages.innerText = ''
+  message.innerText = 'Ходит игрок ' + player
+  for (let i = 0; i < ceil.length; i++) {
+    ceil[i].addEventListener('click', currentStep)
+  }
 })
+
+// Принимает массив и число(ячейка на которую кликнули)
+function checkWin(arr, number) {
+  for (let w = 0, wLen = winCombinations.length; w < wLen; w++) {
+    let someWinArr = winCombinations[w],
+      count = 0
+    if (someWinArr.indexOf(number) !== -1) {
+      for (let k = 0, kLen = someWinArr.length; k < kLen; k++) {
+        if (arr.indexOf(someWinArr[k]) !== -1) {
+          count++
+          if (count === 3) {
+            return true
+          }
+        }
+      }
+      count = 0;
+    }
+  }
+}
