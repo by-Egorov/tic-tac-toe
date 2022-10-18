f00e39163f2c3d95d6e04bedf2110ccc3079b801
Alexander <egorov.dev@gmail.com>
2022-10-18 23:44:50 +0300

    fun

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 let ceil = document.getElementsByClassName("game-item"),
   reset = document.getElementById("reset-game"),
   message = document.getElementById("message"),
+  messages = document.getElementById("messages"),
+  // game = document.querySelector('.game'),
   player = "X",
   stepCount = 0,
   winCombinations = [
     [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    
     [1, 4, 7],
-    [1, 5, 9],
     [2, 5, 8],
     [3, 6, 9],
+    
+    [1, 5, 9],
     [3, 5, 7],
-    [4, 5, 6],
-    [7, 8, 9]
   ],
   dataX = [],
-  dataO = [];
+  dataO = []
 
+// for (let i = 0; i < 9; i++) {
+//   let cell = document.createElement('div')
+//   cell.classList.add('game-item')
+//   game.appendChild(cell)
+// }
 for (let i = 0; i < ceil.length; i++) {
   ceil[i].addEventListener("click", currentStep);
 }
@@ -34,12 +43,12 @@ function currentStep() {
       for (let i = 0; i < ceil.length; i++) {
         ceil[i].removeEventListener("click", currentStep);
       }
-      return (message.innerText = "Победил игрок " + player);
+      return (messages.innerText = "Победил игрок " + player);
     }
     changePlayer();
     stepCount++;
     stepCount === 9
-      ? (message.innerText = "Ничья")
+      ? (messages.innerText = "Ничья")
       : (message.innerText = "Ходит игрок " + player)
   }
 }
@@ -50,23 +59,24 @@ function changePlayer() {
 
 reset.addEventListener("click", function() {
   for (let i = 0; i < ceil.length; i++) {
-    ceil[i].innerText = "";
+    ceil[i].innerText = ''
   }
-  dataO = [];
-  dataX = [];
-  player = "O";
-  stepCount = 0;
-  message.innerText = "Ходит игрок " + player;
+  dataO = []
+  dataX = []
+  player = "X"
+  stepCount = 0
+  messages.innerText = ''
+  message.innerText = "Ходит игрок " + player
   for (let i = 0; i < ceil.length; i++) {
-    ceil[i].addEventListener("click", currentStep);
-    ceil[i].classList.remove("x", "o");
+    ceil[i].addEventListener("click", currentStep)
+    ceil[i].classList.remove("x", "o")
   }
-});
+})
 
 function checkWin(arr, number) {
   for (let w = 0, wLen = winCombinations.length; w < wLen; w++) {
-    let someWinArr = winCombinations[w],
-      count = 0;
+    let someWinArr = winCombinations[w]
+    let count = 0
     if (someWinArr.indexOf(number) !== -1) {
       for (let k = 0, kLen = someWinArr.length; k < kLen; k++) {
         if (arr.indexOf(someWinArr[k]) !== -1) {
